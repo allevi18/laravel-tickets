034c21a1782043b8a9b96d6e0ae7d2aae655b9c2
Rene-Roscher <43115138+Rene-Roscher@users.noreply.github.com>
2020-12-20 19:48:54 +0100

    Update HasConfigModel.php

--- src/Traits/HasConfigModel.php
+++ src/Traits/HasConfigModel.php
@@ -9,23 +9,12 @@ trait HasConfigModel
 
     public function getKeyType()
     {
-        return config('laravel-tickets.models.key-type');
+        return 'string';
     }
 
     public function isIncrementing()
     {
-        return config('laravel-tickets.models.incrementing');
-    }
-    
-    public static function bootHasUuid()
-    {
-        if(config('laravel-tickets.models.uuid')) {
-            static::creating(function ($model) {
-                if (empty($model->id)) {
-                    $model->id = Uuid::uuid4();
-                }
-            });
-        }
+        return !$this instanceof Ticket;
     }
     
 }
