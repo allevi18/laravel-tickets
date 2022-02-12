87964d104be3892a843811fa93b3dca6b4d9693f
allevi18 <48120819+allevi18@users.noreply.github.com>
2022-02-12 19:01:42 +0200

    Update Ticket.php

--- src/Models/Ticket.php
+++ src/Models/Ticket.php
@@ -4,25 +4,55 @@
 namespace RexlManu\LaravelTickets\Models;
 
 
+use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
+use Kyslik\ColumnSortable\Sortable;
+use Nicolaslopezj\Searchable\SearchableTrait;
 use RexlManu\LaravelTickets\Traits\HasConfigModel;
 use RexlManu\LaravelTickets\Traits\TicketId;
+
 
 class Ticket extends Model
 {
     use HasConfigModel;
     use TicketId;
+    use SearchableTrait;
+    use Uuid;
+    use Sortable;
 
     protected $fillable = [
         'subject',
         'priority',
         'state',
         'category_id',
-        'user_id'
+        'user_id',
+        'uuid'
     ];
 
+    protected $searchable = [
+        'columns' => [
+            'tickets.id' => 8,
+            'tickets.uuid' => 10,
+            'users.email' => 10,
+            'tickets.subject' => 5,
+        ],
+        'joins' => [
+            'users' => ['tickets.user_id','users.id'],
+            //'sellers' => ['listings.user_id','listings.id'],
+        ],
+    ];
     protected $appends = ['hash'];
+
+    public $sortable = [
+        'id',
+        'user_id',
+        'category_id',
+        'priority',
+        'state',
+        'created_at',
+        'updated_at'];
+
 
 
     public function getHashAttribute($value) {
