0f13dc035ad8b9d6b28425e1a76c1a63617ca76e
allevi18 <48120819+allevi18@users.noreply.github.com>
2022-02-12 18:50:57 +0200

    Update LaravelTicketsServiceProvider.php

--- src/LaravelTicketsServiceProvider.php
+++ src/LaravelTicketsServiceProvider.php
@@ -1,323 +1,113 @@
 <?php
 
+namespace RexlManu\LaravelTickets;
 
-namespace RexlManu\LaravelTickets\Controllers;
-
-
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
-use Illuminate\View\View;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+use RexlManu\LaravelTickets\Commands\AutoCloseCommand;
 use RexlManu\LaravelTickets\Models\Ticket;
 use RexlManu\LaravelTickets\Models\TicketMessage;
-use RexlManu\LaravelTickets\Models\TicketReference;
 use RexlManu\LaravelTickets\Models\TicketUpload;
-use RexlManu\LaravelTickets\Rule\TicketReferenceRule;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use RexlManu\LaravelTickets\Observers\TicketMessageObserver;
+use RexlManu\LaravelTickets\Observers\TicketObserver;
+use RexlManu\LaravelTickets\Observers\TicketUploadObserver;
+use Hashids;
 
-/**
- * Class TicketController
- *
- * The main logic of the ticket system. All actions are performed here.
- *
- * If the accept header is json, the response will be a json response
- *
- * @package RexlManu\LaravelTickets\Controllers
- */
-trait TicketControllable
+class LaravelTicketsServiceProvider extends ServiceProvider
 {
-
     /**
-     * @link TicketControllable constructor
+     * Bootstrap the application services.
      */
-    public function __construct()
+    public function boot()
     {
-        if (! config('laravel-tickets.permission')) {
-            return;
-        }
+        /*
+         * Optional methods to load your package assets
+         */
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'laravel-tickets');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'laravel-tickets');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->routes();
+        $this->observers();
 
-        $this->middleware(config('laravel-tickets.permissions.list-ticket'))->only('index');
-        $this->middleware(config('laravel-tickets.permissions.create-ticket'))->only('store', 'create');
-        $this->middleware(config('laravel-tickets.permissions.close-ticket'))->only('close');
-        $this->middleware(config('laravel-tickets.permissions.show-ticket'))->only('show');
-        $this->middleware(config('laravel-tickets.permissions.message-ticket'))->only('message');
-        $this->middleware(config('laravel-tickets.permissions.download-ticket'))->only('download');
+        Route::bind('ticket', function ($value, $route) {
+            $id = Hashids::connection('ticket')->decode($value)[0];
+            return Ticket::findOrFail($id);
+        });
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/config.php' => config_path('laravel-tickets.php'),
+            ], 'config');
+
+            // Publishing the views.
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/laravel-tickets'),
+            ], 'views');
+
+            $this->publishes([
+                __DIR__ . '/../database/migrations' => database_path('migrations'),
+            ], 'migrations');
+
+            // Publishing assets.
+            /*$this->publishes([
+                __DIR__.'/../resources/assets' => public_path('vendor/laravel-tickets'),
+            ], 'assets');*/
+
+            // Publishing the translation files.
+            $this->publishes([
+                __DIR__ . '/../resources/lang' => resource_path('lang/vendor/laravel-tickets'),
+            ], 'lang');
+
+            if (config('laravel-tickets.autoclose-days') > 0) {
+                // Registering package commands.
+                $this->commands([ AutoCloseCommand::class ]);
+            }
+        }
     }
 
     /**
-     * Show every @return View|JsonResponse
-     *
-     * @link Ticket that the user has created
-     *
-     * If the accept header is json, the response will be a json response
-     *
+     * Register the application services.
      */
-    public function index()
+    public function register()
     {
-        if (\request()->user()->can(config('laravel-tickets.permissions.all-ticket'))) {
-            $tickets = Ticket::query();
-        } else {
-            $tickets = request()->user()->tickets();
-        }
-        $tickets = $tickets->with('user')->orderBy('id', 'desc')->paginate(10);
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'laravel-tickets');
 
-        return request()->wantsJson() ?
-            response()->json(compact('tickets')) :
-            view('laravel-tickets::tickets.index',
-                compact('tickets')
-            );
+        // Register the main class to use with the facade
+        $this->app->singleton('laravel-tickets', function () {
+            return new LaravelTickets;
+        });
     }
 
-    /**
-     * Show the create form
-     *
-     * @return View
-     */
-    public function create()
+    public function routes()
     {
-        return view('laravel-tickets::tickets.create');
-    }
-
-    /**
-     * Creates a @param Request $request the request
-     *
-     * @return View|JsonResponse|RedirectResponse
-     * @link Ticket
-     *
-     */
-    public function store(Request $request)
-    {
-        $rules = [
-            'subject' => [ 'required', 'string', 'max:191' ],
-            'priority' => [ 'required', Rule::in(config('laravel-tickets.priorities')) ],
-            'message' => [ 'required', 'string' ],
-            'files' => [ 'max:' . config('laravel-tickets.file.max-files') ],
-            'files.*' => [
-                'sometimes',
-                'file',
-                'max:' . config('laravel-tickets.file.size-limit'),
-                'mimes:' . config('laravel-tickets.file.mimetype'),
-            ],
-        ];
-        if (config('laravel-tickets.category')) {
-            $rules[ 'category_id' ] = [
-                'required',
-                Rule::exists(config('laravel-tickets.database.ticket-categories-table'), 'id'),
-            ];
-        }
-        if (config('laravel-tickets.references')) {
-            $rules[ 'reference' ] = [
-                config('laravel-tickets.references-nullable') ? 'nullable' : 'required',
-                new TicketReferenceRule(),
-            ];
-        }
-        $data = $request->validate($rules);
-        if ($request->user()->tickets()->where('state', '!=', 'CLOSED')->count() >= config('laravel-tickets.maximal-open-tickets')) {
-            $message = trans('You have reached the limit of open tickets');
-            return \request()->wantsJson() ?
-                response()->json(compact('message')) :
-                back()->with(
-                    'message',
-                    $message
-                );
-        }
-        $ticket = $request->user()->tickets()->create(
-            $data
-        );
-
-        if (array_key_exists('reference', $data) && $data['reference']) {
-            $reference = explode(',', $data[ 'reference' ]);
-            $ticketReference = new TicketReference();
-            $ticketReference->ticket()->associate($ticket);
-            $ticketReference->referenceable()->associate(
-                resolve($reference[ 0 ])->find($reference[ 1 ])
-            );
-            $ticketReference->save();
-        }
-
-        $ticketMessage = new TicketMessage($data);
-        $ticketMessage->user()->associate($request->user());
-        $ticketMessage->ticket()->associate($ticket);
-        $ticketMessage->save();
-
-        $this->handleFiles($data[ 'files' ] ?? [], $ticketMessage);
-
-        $message = trans('The ticket was successfully created');
-        return $request->wantsJson() ?
-            response()->json(compact('message', 'ticket', 'ticketMessage')) :
-            redirect(route(
-                'laravel-tickets.tickets.show',
-                compact('ticket')
-            ))->with(
-                'message',
-                $message
-            );
-    }
-
-    /**
-     * Show detailed informations about the @param Ticket $ticket
-     *
-     * @return View|JsonResponse|RedirectResponse|void
-     * @link Ticket and the informations
-     *
-     */
-    public function show(Ticket $ticket)
-    {
-        if (! $ticket->user()->get()->contains(\request()->user()) &&
-            ! request()->user()->can(config('laravel-tickets.permissions.all-ticket'))) {
-            return abort(403);
-        }
-
-        $messages = $ticket->messages()->with([ 'user', 'uploads' ])->orderBy('created_at', 'desc');
-
-        return \request()->wantsJson() ?
-            response()->json(compact(
-                'ticket',
-                'messages'
-            )) :
-            view('laravel-tickets::tickets.show',
-                compact(
-                    'ticket',
-                    'messages'
-                )
-            );
-    }
-
-    /**
-     * Send a message to the @param Request $request
-     *
-     * @param Ticket $ticket
-     *
-     * @return JsonResponse|RedirectResponse|void
-     * @link Ticket
-     *
-     */
-    public function message(Request $request, Ticket $ticket)
-    {
-        if (! $ticket->user()->get()->contains(\request()->user()) &&
-            ! request()->user()->can(config('laravel-tickets.permissions.all-ticket'))) {
-            return abort(403);
-        }
-
-        if (! config('laravel-tickets.open-ticket-with-answer') && $ticket->state === 'CLOSED') {
-            $message = trans('You cannot reply to a closed ticket');
-            return \request()->wantsJson() ?
-                response()->json(compact('message')) :
-                back()->with(
-                    'message',
-                    $message
-                );
-        }
-
-        $data = $request->validate([
-            'message' => [ 'required', 'string' ],
-            'files' => [ 'max:' . config('laravel-tickets.file.max-files') ],
-            'files.*' => [
-                'sometimes',
-                'file',
-                'max:' . config('laravel-tickets.file.size-limit'),
-                'mimes:' . config('laravel-tickets.file.mimetype'),
-            ]
-        ]);
-
-        $ticketMessage = new TicketMessage($data);
-        $ticketMessage->user()->associate($request->user());
-        $ticketMessage->ticket()->associate($ticket);
-        $ticketMessage->save();
-
-        $this->handleFiles($data[ 'files' ] ?? [], $ticketMessage);
-
-        $ticket->update([ 'state' => 'OPEN' ]);
-
-        $message = trans('Your answer was sent successfully');
-        return $request->wantsJson() ?
-            response()->json(compact('message')) :
-            back()->with(
-                'message',
-                $message
-            );
-    }
-
-    /**
-     * Declare the @param Ticket $ticket
-     *
-     * @return JsonResponse|RedirectResponse|void
-     * @link Ticket as closed.
-     *
-     */
-    public function close(Ticket $ticket)
-    {
-        if (! $ticket->user()->get()->contains(\request()->user()) &&
-            ! request()->user()->can(config('laravel-tickets.permissions.all-ticket'))) {
-            return abort(403);
-        }
-        if ($ticket->state === 'CLOSED') {
-            $message = trans('The ticket is already closed');
-            return \request()->wantsJson() ?
-                response()->json(compact('message')) :
-                back()->with(
-                    'message',
-                    $message
-                );
-        }
-        $ticket->update([ 'state' => 'CLOSED' ]);
-
-        $message = trans('The ticket was successfully closed');
-        return \request()->wantsJson() ?
-            response()->json(compact('message')) :
-            back()->with(
-                'message',
-                $message
-            );
-    }
-
-    /**
-     * Downloads the file from @param Ticket $ticket
-     *
-     * @param TicketUpload $ticketUpload
-     *
-     * @return BinaryFileResponse
-     * @link TicketUpload
-     *
-     */
-    public function download(Ticket $ticket, TicketUpload $ticketUpload)
-    {
-        if (! $ticket->user()->get()->contains(\request()->user()) &&
-            ! request()->user()->can(config('laravel-tickets.permissions.all-ticket'))) {
-            return abort(403);
-        }
-
-        $storagePath = storage_path('app/' . $ticketUpload->path);
-        if (config('laravel-tickets.pdf-force-preview') && pathinfo($ticketUpload->path, PATHINFO_EXTENSION) === 'pdf') {
-            return response()->file($storagePath);
-        }
-
-        return response()->download($storagePath);
-    }
-
-    /**
-     * Handles the uploaded files for the @param $files array uploaded files
-     *
-     * @param TicketMessage $ticketMessage
-     *
-     * @link TicketMessage
-     *
-     */
-    private function handleFiles($files, TicketMessage $ticketMessage)
-    {
-        if (! config('laravel-tickets.files') || $files == null) {
-            return;
-        }
-        foreach ($files as $file) {
-            $ticketMessage->uploads()->create([
-                'path' => $file->storeAs(
-                    config('laravel-tickets.file.path') . $ticketMessage->id,
-                    $file->getClientOriginalName(),
-                    config('laravel-tickets.file.driver')
-                )
-            ]);
+        // Macro routing
+        foreach ([ 'support', 'tickets' ] as $routeMacroName) {
+            Router::macro($routeMacroName, function ($controller) use($routeMacroName) {
+                Route::middleware(config('laravel-tickets.guard'))->name('laravel-tickets.')->group(function () use ($controller, $routeMacroName) {
+                    Route::prefix($routeMacroName)->group(function () use ($controller, $routeMacroName) {
+                        Route::get('/', [ $controller, 'index' ])->name($routeMacroName.'.index');
+                        Route::post('/', [ $controller, 'store' ])->name($routeMacroName.'.store');
+                        Route::get('/create', [ $controller, 'create' ])->name($routeMacroName.'.create');
+                        Route::prefix('{ticket}')->group(function () use ($controller, $routeMacroName) {
+                            Route::get('/', [ $controller, 'show' ])->name($routeMacroName.'.show');
+                            Route::post('/', [ $controller, 'close' ])->name($routeMacroName.'.close');
+                            Route::post('/message', [ $controller, 'message' ])->name($routeMacroName.'.message');
+                            Route::prefix('{ticketUpload}')->group(function () use ($controller, $routeMacroName) {
+                                Route::get('/download', [ $controller, 'download' ])->name($routeMacroName.'.download');
+                            });
+                        });
+                    });
+                });
+            });
         }
     }
 
+    private function observers()
+    {
+        Ticket::observe(TicketObserver::class);
+        TicketMessage::observe(TicketMessageObserver::class);
+        TicketUpload::observe(TicketUploadObserver::class);
+    }
 }
