b40443eb010b3e5186543a01c5d6fb8f8ed01d39
allevi18 <48120819+allevi18@users.noreply.github.com>
2022-02-16 15:59:57 +0200

    Update TicketCategory.php

--- src/Models/TicketCategory.php
+++ src/Models/TicketCategory.php
@@ -4,6 +4,7 @@
 namespace RexlManu\LaravelTickets\Models;
 
 
+use App\Models\TicketModel;
 use Illuminate\Database\Eloquent\Model;
 use RexlManu\LaravelTickets\Traits\HasConfigModel;
 
@@ -13,11 +14,22 @@ class TicketCategory extends Model
     use HasConfigModel;
 
     protected $fillable = [
-        'translation'
+        'translation, priority'
     ];
 
     public function getTable()
     {
         return config('laravel-tickets.database.ticket-categories-table');
     }
+
+    public function models()
+    {
+        return $this->belongsToMany(
+            TicketModel::class,
+            'ticket_category_permissions',
+            'category_id',
+            'model_id'
+        );
+    }
+
 }
